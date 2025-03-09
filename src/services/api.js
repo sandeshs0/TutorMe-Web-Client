25e85d3cc756c0b80a38573c19fb6955b3f10ff5
sandeshs0 <hellosandesh0@gmail.com>
2025-03-10 00:41:53 +0545

    added deployed server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://tutorme-web-server.onrender.com",
 });
 
 API.interceptors.request.use(
